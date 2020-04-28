b0e7a55f12608a37fb106f916e6b500f2958e917
Brendon Barrios <brbarrios@cpp.edu>
2020-04-28 01:19:37 -0700

    adds preliminary code for initializing a screen

--- Clock.py
+++ Clock.py
@@ -1,2 +1,13 @@
 import curses
 import time
+
+print("intializing screen")
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+
+time.sleep(10)
+
+curses.nocbreak(); screen.keypad(0); curses.echo()
+curses.endwin()
+print("screen closed")
