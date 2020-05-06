fc754cc78e1637d7a42776be98739d7e96b301f3
jrdmnts1 <64439397+jrdmnts1@users.noreply.github.com>
2020-05-06 01:53:33 -0700

    added clock gui

--- test.py
+++ test.py
@@ -72,7 +72,7 @@ print("testing 2")
 
 
 # ===============================================================
-#Jarod: Testing GPIO library
+# Jarod: Testing GPIO library
 # ===============================================================
 # this method is  is waiting for a rising edge from a button Press
 # try this Out
@@ -140,3 +140,36 @@ while True:
     #deg_c, deg_f = read_temp()
     print(read_temp())
     time.sleep(1)
+# ===============================================================
+# Jarod: Testing clock GUI
+# ===============================================================
+from tkinter import * 
+from tkinter.ttk import *
+  
+# importing strftime function to 
+# retrieve system's time 
+from time import strftime 
+  
+# creating tkinter window 
+root = Tk() 
+root.title('Clock') 
+  
+# This function is used to  
+# display time on the label 
+def time(): 
+    string = strftime('%H:%M:%S %p') 
+    lbl.config(text = string) 
+    lbl.after(1000, time) 
+  
+# Styling the label widget so that clock 
+ 
+lbl = Label(root, font = ('calibri', 40, 'bold'), 
+            background = 'purple', 
+            foreground = 'white') 
+  
+# Placing clock at the centre 
+# of the tkinter window 
+lbl.pack(anchor = 'center') 
+time() 
+  
+mainloop() 
