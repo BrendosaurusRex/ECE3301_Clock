58a7661d638f5e4d1113d065cc7c82ab15e72f30
Brendon Barrios <brbarrios@cpp.edu>
2020-05-07 13:14:13 -0700

    adds temp sensor functions

--- Clock.py
+++ Clock.py
@@ -3,6 +3,36 @@ import tkinter
 import time
 import calendar
 from datetime import date
+import glob
+import os
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28-*')[0] # Should be "28*" as device directory but
+device_file = device_folder + '/w1_slave'       # hardware is not interfacing properly
+
+def read_temp_raw():
+    # If device_file magically appears and is consistent, then use this code block
+     f = open(device_file, 'r')
+     lines = f.readlines()
+     f.close()
+     return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+
 cal = calendar.month(2020, 5)
 root = Tk()
 frame = Frame(root)
