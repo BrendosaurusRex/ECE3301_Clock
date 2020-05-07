5b0728f1bdff10112a8346ddac1f8e77c47e8070
jrdmnts1 <64439397+jrdmnts1@users.noreply.github.com>
2020-05-07 10:07:06 -0700

    edit gui

--- test.py
+++ test.py
@@ -144,33 +144,67 @@ def read_temp():
 # Jarod: Testing clock GUI
 # ===============================================================
 from tkinter import *
-from tkinter.ttk import *
-
-# importing strftime function to
-# retrieve system's time
-from time import strftime
-
-# creating tkinter window
+import tkinter
+import time
+import calendar
+from datetime import date
+cal = calendar.month(2020, 5)
 root = Tk()
-root.title('Clock')
+frame = Frame(root)
+frame.pack()
 
-# This function is used to
-# display time on the label
-def time():
-    string = strftime('%H:%M:%S %p')
-    lbl.config(text = string)
-    lbl.after(1000, time)
+root.geometry('800x400+350+300')
+root.title("python clock designing")
+ 
 
-# Styling the label widget so that clock
+cframe =Frame(root, width=200, height=100, bg='green')
+cframe.pack(side= LEFT)
+ 
+clock=Label(cframe, padx=200, pady=100, bd=3, fg= 'white',font=('arial',20,'bold'),text=cal ,bg='light green')
+clock.pack(side= LEFT )
 
-lbl = Label(root, font = ('calibri', 40, 'bold'),
-            background = 'purple',
-            foreground = 'white')
+# bottomframe = Frame(root)
+# bottomframe.pack( side = BOTTOM )
+# 
+# redbutton = Button(frame, text=cal, fg="red")
+# redbutton.pack( side =  RIGHT)
+# 
+# greenbutton = Button(frame, text="Brown", fg="brown")
+# greenbutton.pack( side = LEFT )
+# 
+# bluebutton = Button(frame, text="Blue", fg="blue")
+# bluebutton.pack( side = LEFT )
+# 
+# blackbutton = Button(bottomframe, text="Black", fg="black")
+# blackbutton.pack( side = BOTTOM)
 
-# Placing clock at the centre
-# of the tkinter window
-lbl.pack(anchor = 'center')
-time()
+today = date.today()
 
+cframe3 =Frame(root, width=200, height=100, bg='green')
+cframe3.pack(side=BOTTOM)
+ 
+clock3=Label(cframe, padx=200, pady=100, bd=3, fg= 'white',font=('arial',20,'bold'),text=today ,bg='light green')
+clock3.pack(side= BOTTOM )
+
+timenow=''
+c2frame =Frame(root, width=200, height=100)
+c2frame.pack(side = LEFT)
+ 
+clock2=Label(cframe,padx=200, pady=200, bd=3, fg= 'white',font=('arial',48,'bold'),text= timenow,bg='black')
+clock2.pack(side = LEFT)
+ 
+
+
+
+def timer():
+    global timenow
+    newtime = time.strftime('%H: %M: %S %p')
+    if newtime != timenow:
+        timenow= newtime
+        clock2.config(text= timenow)
+    clock2.after(200, timer)
+timer()
+ 
+ 
 mainloop()
-#IO
+
