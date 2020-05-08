e188a5c368e972c574e1ba74f538e5e36f32884b
jrdmnts1 <64439397+jrdmnts1@users.noreply.github.com>
2020-05-07 20:00:54 -0700

    again

--- test.py
+++ test.py
@@ -92,6 +92,72 @@ GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
 
 message = input("press enter to quit\n\n")
 GPIO.cleanup()
+# ===============================================================
+# Jarod: Testing clock GUI
+# ===============================================================
+from tkinter import *
+import tkinter
+import time
+import calendar
+from datetime import date
+
+cal = calendar.month(2020, 5)
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+root.geometry('600x300+350+300')
+root.title("python clock designing")
+
+
+cframe =Frame(root, width=15, height=15, bg='black')
+cframe.pack(side= LEFT)
+ 
+clock=Label(cframe, padx=15, pady=15, bd=3, fg= 'white',font=('arial',20,'bold'),text=cal ,bg='black')
+clock.pack(side= LEFT )
+
+
+
+
+deg_c,deg_f=read_temp()
+
+cframe4 =Frame(root, width=200, height=31, bg='black')
+cframe4.pack(side=BOTTOM)
+ 
+clock4=Label(cframe, padx=200, pady=31, bd=3, fg= 'white',font=('arial',20,'bold'),text=(temp_f,"F°","/",temp_c,"C°") ,bg='black')
+clock4.pack(side= BOTTOM )
+
+
+today = date.today()
+
+cframe3 =Frame(root, width=200, height=25, bg='black')
+cframe3.pack(side=BOTTOM)
+ 
+clock3=Label(cframe, padx=200, pady=25, bd=3, fg= 'white',font=('arial',20,'bold'),text=today ,bg='black')
+clock3.pack(side= BOTTOM )
+
+timenow=''
+c2frame =Frame(root, width=200, height=35,bg='black')
+c2frame.pack(side = LEFT)
+ 
+clock2=Label(cframe,padx=200, pady=35, bd=3, fg= 'white',font=('arial',20,'bold'),text= timenow,bg='black')
+clock2.pack(side = LEFT)
+ 
+
+
+
+def timer():
+    global timenow
+    newtime = time.strftime('%H: %M: %S %p')
+    if newtime != timenow:
+        timenow= newtime
+        clock2.config(text= timenow)
+    clock2.after(200, timer)
+timer()
+
+ 
+ 
+mainloop()
 
 
 # ===============================================================
@@ -140,71 +206,4 @@ def read_temp():
     #deg_c, deg_f = read_temp()
     print(read_temp())
     time.sleep(1)
-# ===============================================================
-# Jarod: Testing clock GUI
-# ===============================================================
-from tkinter import *
-import tkinter
-import time
-import calendar
-from datetime import date
-cal = calendar.month(2020, 5)
-root = Tk()
-frame = Frame(root)
-frame.pack()
-
-root.geometry('800x400+350+300')
-root.title("python clock designing")
-
-
-cframe =Frame(root, width=200, height=100, bg='green')
-cframe.pack(side= LEFT)
-
-clock=Label(cframe, padx=200, pady=100, bd=3, fg= 'white',font=('arial',20,'bold'),text=cal ,bg='light green')
-clock.pack(side= LEFT )
-
-# bottomframe = Frame(root)
-# bottomframe.pack( side = BOTTOM )
-#
-# redbutton = Button(frame, text=cal, fg="red")
-# redbutton.pack( side =  RIGHT)
-#
-# greenbutton = Button(frame, text="Brown", fg="brown")
-# greenbutton.pack( side = LEFT )
-#
-# bluebutton = Button(frame, text="Blue", fg="blue")
-# bluebutton.pack( side = LEFT )
-#
-# blackbutton = Button(bottomframe, text="Black", fg="black")
-# blackbutton.pack( side = BOTTOM)
-
-today = date.today()
-
-cframe3 =Frame(root, width=200, height=100, bg='green')
-cframe3.pack(side=BOTTOM)
-
-clock3=Label(cframe, padx=200, pady=100, bd=3, fg= 'white',font=('arial',20,'bold'),text=today ,bg='light green')
-clock3.pack(side= BOTTOM )
-
-timenow=''
-c2frame =Frame(root, width=200, height=100)
-c2frame.pack(side = LEFT)
-
-clock2=Label(cframe,padx=200, pady=200, bd=3, fg= 'white',font=('arial',48,'bold'),text= timenow,bg='black')
-clock2.pack(side = LEFT)
-
-
-
-
-def timer():
-    global timenow
-    newtime = time.strftime('%H: %M: %S %p')
-    if newtime != timenow:
-        timenow= newtime
-        clock2.config(text= timenow)
-    clock2.after(200, timer)
-timer()
-
-
-mainloop()
-#IO
+ 
