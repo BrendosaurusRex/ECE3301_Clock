3f6ac9b89c81a250ca920f3a56627c082d66a5cd
Brendon Barrios <brbarrios@cpp.edu>
2020-06-16 12:26:45 -0700

    removes temp functions and Old Code

--- Clock.py
+++ Clock.py
@@ -3,35 +3,6 @@ import tkinter
 import time
 import calendar
 from datetime import date
-import glob
-import os
-
-# os.system('modprobe w1-gpio')
-# os.system('modprobe w1-therm')
-#
-# base_dir = '/sys/bus/w1/devices/'
-# device_folder = glob.glob(base_dir + '28-*')[0] # Should be "28*" as device directory but
-# device_file = device_folder + '/w1_slave'       # hardware is not interfacing properly
-#
-# def read_temp_raw():
-#     # If device_file magically appears and is consistent, then use this code block
-#      f = open(device_file, 'r')
-#      lines = f.readlines()
-#      f.close()
-#      return lines
-#
-# def read_temp():
-#     lines = read_temp_raw()
-#     while lines[0].strip()[-3:] != 'YES':
-#         time.sleep(0.2)
-#         lines = read_temp_raw()
-#     equals_pos = lines[1].find('t=')
-#     if equals_pos != -1:
-#         temp_string = lines[1][equals_pos+2:]
-#         temp_c = float(temp_string) / 1000.0
-#         temp_f = temp_c * 9.0 / 5.0 + 32.0
-#         return temp_c, temp_f
-
 
 cal = calendar.month(2020, 5)
 root = Tk()
@@ -77,25 +48,3 @@ timer()
 
 
 mainloop()
-
-
-# =============================================================================
-# Old Code
-# =============================================================================
-
-# def main(stdscr):
-#     curses.curs_set(0)
-#     stdscr.nodelay(1)
-#
-#     dti = mktime(datetime.now().timetuple())
-#     while 1:
-#         ndti = mktime(datetime.now().timetuple())
-#         if dti < ndti:
-#             dti = ndti
-#             stdscr.addstr(datetime.now().strftime('%b %d  %H:%M:%S\r'))
-#             stdscr.refresh()
-#             sleep(0.95)
-#         else:
-#             sleep(0.01)
-#
-# curses.wrapper(main)
